c4efb2e5eaae8e5a51aa4a57b4f23f3742c0391c
Sergio Garcia <segarciat314@gmail.com>
2025-04-25 15:31:08 -0400

    Apply small fixes toprevious lectures

--- package_notes.sh
+++ package_notes.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-# Creates a compressed tarball of the lecture notes, including .tex, .png,
-# and image files.
+# Creates a compressed tarball of the lecture notes.
+# Includes .pdf, .tex, .bib, and image files.
 
 shopt -s globstar
 tar -f notes.tar.gz -cvz notes/**/*.{pdf,tex,png,jpg,bib}
